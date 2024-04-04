7a5ec409902d22f57cb5c4ea9dcebff5ff77a12f
hingchou <hing_chou@163.com>
2024-04-04 23:55:30 +0800

    增加输入框

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,25 @@
 // 1. 导入必要的库和样式
-import { Button, Rows, Text } from "@canva/app-ui-kit"; // 1.1 导入Canva的UI组件库
+import { Button, Rows, Text, TextInput } from "@canva/app-ui-kit"; // 1.1 导入Canva的UI组件库
 import { addNativeElement } from "@canva/design"; // 1.2 导入Canva的设计元素库
 import * as React from "react"; // 1.3 导入React库
 import styles from "styles/components.css"; // 1.4 导入样式文件
 
 // 2. 定义App组件
 export const App = () => { // 2.1 定义App函数组件
+  // 2.2 定义一个状态变量来存储输入框的值
+  const [inputValue, setInputValue] = React.useState('');
+
   // 3. 定义点击事件处理函数
   const onClick = () => { // 3.1 定义onClick函数
     addNativeElement({ // 3.2 在点击事件中添加一个原生元素
       type: "TEXT", // 3.3 元素类型为文本
-      children: ["Hello world!"], // 3.4 元素的内容为"Hello world!"
+      children: [inputValue], // 3.4 元素的内容为输入框的值
     });
+  };
+
+  // 3.5 定义一个处理输入框值改变的函数
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
   };
 
   // 4. 返回JSX元素
@@ -21,28 +29,11 @@ export const App = () => { // 2.1 定义App函数组件
         <Text> 
         Enter your text, leave the rest to us.
         </Text>
+        <TextInput value={inputValue} onChange={onInputChange} stretch /> {/* 4.2 在Text和Button之间添加一个TextInput组件 */}
         <Button variant="primary" onClick={onClick} stretch> 
           start
         </Button>
       </Rows>
     </div>
   );
-};
-
-// 5. 使用d3-cloud生成词云
-const generateWordCloud = (words) => { // 5.1 定义generateWordCloud函数，参数为词汇数组
-  const layout = d3Cloud() // 5.2 创建一个新的词云布局
-    .size([500, 500]) // 5.3 设置词云的大小
-    .words(words.map((d) => ({ text: d }))) // 5.4 设置词云的词汇
-    .padding(5) // 5.5 设置词汇之间的间距
-    .rotate(() => ~~(Math.random() * 2) * 90) // 5.6 设置词汇的旋转角度
-    .fontSize(20) // 5.7 设置词汇的字体大小
-    .on("end", draw); // 5.8 当布局完成时，调用draw函数来绘制词云
-
-  layout.start(); // 5.9 开始生成词云布局
-
-  function draw(words) { // 5.10 定义draw函数，参数为词汇数组
-    // 在这里，你需要使用D3或其他库来绘制词云
-    // 例如，你可以创建一个SVG元素，然后为每个词汇创建一个text元素
-  }
 };
