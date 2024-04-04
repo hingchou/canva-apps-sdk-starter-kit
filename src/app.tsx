c7a670b7e9757fe448a2ced2d4cad8e970d76164
hingchou <hing_chou@163.com>
2024-04-04 23:19:08 +0800

    加载D3词云库

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { Button, Rows, Text } from "@canva/app-ui-kit"; // 1.1 导入Canva的UI�
 import { addNativeElement } from "@canva/design"; // 1.2 导入Canva的设计元素库
 import * as React from "react"; // 1.3 导入React库
 import styles from "styles/components.css"; // 1.4 导入样式文件
+import * as d3Cloud from 'd3-cloud';
 
 // 2. 定义App组件
 export const App = () => { // 2.1 定义App函数组件
@@ -28,4 +29,22 @@ export const App = () => { // 2.1 定义App函数组件
       </Rows>
     </div>
   );
+};
+
+// 5. 使用d3-cloud生成词云
+const generateWordCloud = (words) => { // 5.1 定义generateWordCloud函数，参数为词汇数组
+  const layout = d3Cloud() // 5.2 创建一个新的词云布局
+    .size([500, 500]) // 5.3 设置词云的大小
+    .words(words.map((d) => ({ text: d }))) // 5.4 设置词云的词汇
+    .padding(5) // 5.5 设置词汇之间的间距
+    .rotate(() => ~~(Math.random() * 2) * 90) // 5.6 设置词汇的旋转角度
+    .fontSize(20) // 5.7 设置词汇的字体大小
+    .on("end", draw); // 5.8 当布局完成时，调用draw函数来绘制词云
+
+  layout.start(); // 5.9 开始生成词云布局
+
+  function draw(words) { // 5.10 定义draw函数，参数为词汇数组
+    // 在这里，你需要使用D3或其他库来绘制词云
+    // 例如，你可以创建一个SVG元素，然后为每个词汇创建一个text元素
+  }
 };
